99c00338432f3a909ef76f6d150960c851679d8c
alqu7095 <alfredoquintanacato@gmail.com>
2019-12-04 20:02:44 -0500

    Update room.py

--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
+from typing import Optional, List
+from item import Item
+from dataclasses import dataclass, field
+
+@dataclass
+class Room:
+    name: str
+    description: str
+    n_to: Optional[str] = None
+    s_to: Optional[str] = None
+    e_to: Optional[str] = None
+    w_to: Optional[str] = None
+    items: List[Item] = field(default_factory=list)
